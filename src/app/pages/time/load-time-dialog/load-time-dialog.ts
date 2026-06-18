c14d174cd7114301098a7a2c90530be0693b4bc1
JPP Git <jppasquinelli@gmail.com>
2026-06-18 19:16:21 -0300

    fix modal load time

--- src/app/pages/time/load-time-dialog/load-time-dialog.ts
+++ src/app/pages/time/load-time-dialog/load-time-dialog.ts
@@ -3,7 +3,10 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { OrdersService, Order } from '../../../services/orders.service';
 import { TimeService } from '../../../services/time.service';
-import { Subject, debounceTime, distinctUntilChanged, switchMap, of } from 'rxjs';
+import { Subject, debounceTime, distinctUntilChanged, switchMap, of, map, startWith } from 'rxjs';
+import { TasksTypeService, TaskType } from '../../../services/tasks-type.service';
+import { TaskTypeDialog } from '../../tareas/task-type-dialog/task-type-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-load-time-dialog',
@@ -15,16 +18,23 @@ export class LoadTimeDialog implements OnInit {
   orderCtrl = new FormControl(null, Validators.required);
   filteredOrders: Order[] = [];
 
+  taskTypeCtrl = new FormControl();
+  taskTypes: TaskType[] = [];
+  filteredTaskTypes: TaskType[] = [];
+
   timeForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<LoadTimeDialog>,
     private ordersService: OrdersService,
     private timeService: TimeService,
+    private tasksTypeService: TasksTypeService,
+    private dialog: MatDialog,
     private fb: FormBuilder
   ) {
     this.timeForm = this.fb.group({
       order: this.orderCtrl,
+      taskType: this.taskTypeCtrl,
       timeValue: [null, [Validators.required, Validators.min(0.1)]],
       timeUnit: ['hs', Validators.required]
     });
@@ -46,6 +56,48 @@ export class LoadTimeDialog implements OnInit {
         this.filteredOrders = response.items;
       }
     });
+
+    this.loadTaskTypes();
+
+    this.taskTypeCtrl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filterTaskTypes(name as string) : this.taskTypes.slice();
+      })
+    ).subscribe(filtered => {
+      this.filteredTaskTypes = filtered;
+    });
+  }
+
+  loadTaskTypes() {
+    this.tasksTypeService.findAll().subscribe(types => {
+      this.taskTypes = types;
+      // Trigger valueChanges to update the filtered list
+      this.taskTypeCtrl.setValue(this.taskTypeCtrl.value);
+    });
+  }
+
+  private _filterTaskTypes(name: string): TaskType[] {
+    const filterValue = name.toLowerCase();
+    return this.taskTypes.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  displayTaskTypeFn(taskType: TaskType): string {
+    return taskType && taskType.name ? taskType.name : '';
+  }
+
+  openTaskTypeDialog() {
+    const dialogRef = this.dialog.open(TaskTypeDialog, {
+      width: '400px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.loadTaskTypes();
+      }
+    });
   }
 
   displayFn(order: Order): string {
@@ -65,6 +117,7 @@ export class LoadTimeDialog implements OnInit {
       this.timeService.createTime({
         minutes: minutes,
         orderId: order.id,
+        taskTypeId: this.timeForm.value.taskType?.id || undefined
       }).subscribe({
         next: () => {
           this.dialogRef.close(true);
